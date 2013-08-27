c38c61932b687d924874ad2dcdab9ee57246e9c2
Knut <kiesel@cern.ch>
2013-08-27 10:04:01 +0200

    moved lumimask from rawMulticrab to generateMulticrab

--- susyNtuplizer/createMulticrabConfiguration.py
+++ susyNtuplizer/createMulticrabConfiguration.py
@@ -13,6 +13,7 @@ total_number_of_lumis = -1
 lumis_per_job = 200
 
 pset = os.environ['CMSSW_BASE']+"/src/SUSYPhotonAnalysis/SusyNtuplizer/runOverAOD_CommandLine.py"
+lumiPath = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt"
 
 conf = ConfigParser.SafeConfigParser()
 conf.read( inputFile )
@@ -30,10 +31,10 @@ for section in conf.sections():
     output += "CMSSW.datasetpath = %s\n"%conf.get( section, "datasetpath" )
     output += "CMSSW.pset = %s\n"%pset
     output += "CMSSW.pycfg_params = dataset=%s\n"%conf.get( section, "dataset" )
-    if conf.has_option( section, "lumi_mask" ):
+    if "Run20" in conf.get( section, "datasetpath" ):
         output += "CMSSW.total_number_of_lumis = %s\n"%total_number_of_lumis
         output += "CMSSW.lumis_per_job = %s\n"%lumis_per_job
-        output += "CMSSW.lumi_mask = %s\n"%conf.get( section, "lumi_mask" )
+        output += "CMSSW.lumi_mask = %s\n"%lumiPath
     else:
         output += "CMSSW.total_number_of_events = %s\n"%total_number_of_events
         output += "CMSSW.events_per_job = %s\n"%events_per_job
